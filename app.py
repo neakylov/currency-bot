9acabb9ad26231828478bd2f3f637c2c84d80892
neakylov <akula.kirya@inbox.ru>
2023-03-12 19:51:07 +0300

    Basic Currency Bot

--- app.py
+++ app.py
@@ -11,4 +11,4 @@ async def main():
 if __name__ == '__main__':
     asyncio.run(main())
 
-    #asyncio.get_event_loop().run_until_complete(main()) - if asyncio.run() caused an error
+    #asyncio.get_event_loop().run_until_complete(main()) # - if asyncio.run() caused an error
