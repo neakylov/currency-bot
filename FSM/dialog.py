f2a3a1a68ecf348995a890faa70f2c5e2ff4a3be
neakylov <akula.kirya@inbox.ru>
2023-03-15 19:03:13 +0300

    Basic Currency Bot

--- FSM/dialog.py
+++ FSM/dialog.py
@@ -11,27 +11,27 @@ from keyboards.default.start_buttons import start_buttons, cancel_button
 
 
 class Converter(StatesGroup):
-    first_currency = State()
+    first_value = State()
     quantity = State()
-    second_currency = State()
+    second_value = State()
 
 
-rates = ['USD', 'AED', 'AFN', 'ALL', 'AMD', 'ANG', 'AOA', 'ARS', 'AUD', 'AWG', 'AZN', 'BAM', 'BBD', 'BDT', 'BGN', 'BHD',
-         'BIF', 'BMD', 'BND', 'BOB', 'BRL', 'BSD', 'BTN', 'BWP', 'BYN', 'BZD', 'CAD', 'CDF', 'CHF', 'CLP', 'CNY', 'COP',
-         'CRC', 'CUP', 'CVE', 'CZK', 'DJF', 'DKK', 'DOP', 'DZD', 'EGP', 'ERN', 'ETB', 'EUR', 'FJD', 'FKP', 'FOK', 'GBP',
-         'GEL', 'GGP', 'GHS', 'GIP', 'GMD', 'GNF', 'GTQ', 'GYD', 'HKD', 'HNL', 'HRK', 'HTG', 'HUF', 'IDR', 'ILS', 'IMP',
-         'INR', 'IQD', 'IRR', 'ISK', 'JEP', 'JMD', 'JOD', 'JPY', 'KES', 'KGS', 'KHR', 'KID', 'KMF', 'KRW', 'KWD', 'KYD',
-         'KZT', 'LAK', 'LBP', 'LKR', 'LRD', 'LSL', 'LYD', 'MAD', 'MDL', 'MGA', 'MKD', 'MMK', 'MNT', 'MOP', 'MRU', 'MUR',
-         'MVR', 'MWK', 'MXN', 'MYR', 'MZN', 'NAD', 'NGN', 'NIO', 'NOK', 'NPR', 'NZD', 'OMR', 'PAB', 'PEN', 'PGK', 'PHP',
-         'PKR', 'PLN', 'PYG', 'QAR', 'RON', 'RSD', 'RUB', 'RWF', 'SAR', 'SBD', 'SCR', 'SDG', 'SEK', 'SGD', 'SHP', 'SLE',
-         'SLL', 'SOS', 'SRD', 'SSP', 'STN', 'SYP', 'SZL', 'THB', 'TJS', 'TMT', 'TND', 'TOP', 'TRY', 'TTD', 'TVD', 'TWD',
-         'TZS', 'UAH', 'UGX', 'UYU', 'UZS', 'VES', 'VND', 'VUV', 'WST', 'XAF', 'XCD', 'XDR', 'XOF', 'XPF', 'YER', 'ZAR',
-         'ZMW', 'ZWL']
+currencies = ['USD', 'AED', 'AFN', 'ALL', 'AMD', 'ANG', 'AOA', 'ARS', 'AUD', 'AWG', 'AZN', 'BAM', 'BBD', 'BDT', 'BGN',
+              'BHD', 'BIF', 'BMD', 'BND', 'BOB', 'BRL', 'BSD', 'BTN', 'BWP', 'BYN', 'BZD', 'CAD', 'CDF', 'CHF', 'CLP',
+              'CNY', 'COP', 'CRC', 'CUP', 'CVE', 'CZK', 'DJF', 'DKK', 'DOP', 'DZD', 'EGP', 'ERN', 'ETB', 'EUR', 'FJD',
+              'FKP', 'FOK', 'GBP', 'GEL', 'GGP', 'GHS', 'GIP', 'GMD', 'GNF', 'GTQ', 'GYD', 'HKD', 'HNL', 'HRK', 'HTG',
+              'HUF', 'IDR', 'ILS', 'IMP', 'INR', 'IQD', 'IRR', 'ISK', 'JEP', 'JMD', 'JOD', 'JPY', 'KES', 'KGS', 'KHR',
+              'KID', 'KMF', 'KRW', 'KWD', 'KYD', 'KZT', 'LAK', 'LBP', 'LKR', 'LRD', 'LSL', 'LYD', 'MAD', 'MDL', 'MGA',
+              'MKD', 'MMK', 'MNT', 'MOP', 'MRU', 'MUR', 'MVR', 'MWK', 'MXN', 'MYR', 'MZN', 'NAD', 'NGN', 'NIO', 'NOK',
+              'NPR', 'NZD', 'OMR', 'PAB', 'PEN', 'PGK', 'PHP', 'PKR', 'PLN', 'PYG', 'QAR', 'RON', 'RSD', 'RUB', 'RWF',
+              'SAR', 'SBD', 'SCR', 'SDG', 'SEK', 'SGD', 'SHP', 'SLE', 'SLL', 'SOS', 'SRD', 'SSP', 'STN', 'SYP', 'SZL',
+              'THB', 'TJS', 'TMT', 'TND', 'TOP', 'TRY', 'TTD', 'TVD', 'TWD', 'TZS', 'UAH', 'UGX', 'UYU', 'UZS', 'VES',
+              'VND', 'VUV', 'WST', 'XAF', 'XCD', 'XDR', 'XOF', 'XPF', 'YER', 'ZAR', 'ZMW', 'ZWL']
 
 
 @dp.message(Text('Currency converter'))
 async def converter(message: types.Message, state: FSMContext) -> None:
-    await state.set_state(Converter.first_currency)
+    await state.set_state(Converter.first_value)
     await message.answer(f'Select the currency you want to convert.\nExample: \"USD\".',
                          reply_markup=cancel_button())
 
@@ -45,14 +45,14 @@ async def cancel_handler(message: types.Message, state: FSMContext) -> None:
     await message.answer('Cancelled', reply_markup=start_buttons())
 
 
-@dp.message(Converter.first_currency)
+@dp.message(Converter.first_value)
 async def first(message: types.Message, state: FSMContext) -> None:
-    if message.text.upper() in rates:
+    if message.text.upper() in currencies:
         await state.update_data(first_currency=message.text.upper())
         await state.set_state(Converter.quantity)
         await message.answer('Enter the quantity.\nExample: 13.44')
     else:
-        await state.set_state(Converter.first_currency)
+        await state.set_state(Converter.first_value)
         await message.answer(f'Error: InvalidCurrency. Try again.')
 
 
@@ -72,24 +72,24 @@ async def enter_quantity(message: types.Message, state: FSMContext) -> None:
         await message.answer('The message must be a number')
     else:
         await state.update_data(quantity=float(result))
-        await state.set_state(Converter.second_currency)
+        await state.set_state(Converter.second_value)
         await message.answer('Choose the currency you will receive.\nExample: \"EUR\".')
 
 
-@dp.message(Converter.second_currency)
+@dp.message(Converter.second_value)
 async def second(message: types.Message, state: FSMContext) -> None:
-    if message.text.upper() in rates:
+    if message.text.upper() in currencies:
         data = await state.update_data(second_currency=message.text.upper())
         await state.clear()
         await converted(message=message, data=data)
     else:
-        await state.set_state(Converter.second_currency)
+        await state.set_state(Converter.second_value)
         await message.answer(f'Error: InvalidCurrency. Try again.')
 
 
 async def converted(message: types.Message, data: Dict[str, Any]) -> None:
     text = message.text.upper()
-    first_curr = data['first_currency']
+    first_curr = data['first_value']
     response = requests.get(config.URL).json()
     from_curr = response.get('conversion_rates')[first_curr]
     to_curr = response.get('conversion_rates')[text]
