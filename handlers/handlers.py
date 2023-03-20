36aac3111955ef6fcd3455aa3ca1e07f99822219
neakylov <akula.kirya@inbox.ru>
2023-03-20 16:03:19 +0300

    Добавление FSM

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -7,6 +7,16 @@ from aiogram.filters import Text
 from data import config
 from keyboards.default.start_buttons import start_buttons
 from keyboards.inline.pagination import pagination
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
+
+
+class Language(StatesGroup):
+    lang = State()
+
+
+class ExchangeRate(StatesGroup):
+    text = State()
 
 
 def get_data():
@@ -28,24 +38,28 @@ async def cmd_start(message: types.Message):
 
 
 @dp.message(Text('Курс валют'))
-async def exchange(message: types.Message):
+async def exchange(message: types.Message, state: FSMContext):
     text = get_data()
+    await state.set_state(ExchangeRate.text)
+    await state.update_data(text=text)
     await message.reply(''.join(text[:25]), reply_markup=pagination(page=1))
 
 
 @dp.callback_query(Text(startswith='page_'))
-async def callbacks_page(callback: types.CallbackQuery):
+async def callbacks_page(callback: types.CallbackQuery, state: FSMContext):
     data = callback.data.split('_')
     action = data[1]
     page = int(data[2])
     if action == 'next':
-        text = get_data()
+        data_text = await state.get_data()
+        text = data_text['text']
         new_page = page + 1
         second_item = 25 * new_page
         first_item = second_item - 25
         await callback.message.edit_text(''.join(text[first_item:second_item]), reply_markup=pagination(page=new_page))
     elif action == 'back':
-        text = get_data()
+        data_text = await state.get_data()
+        text = data_text['text']
         new_page = page - 1
         second_item = 25 * new_page
         first_item = second_item - 25
