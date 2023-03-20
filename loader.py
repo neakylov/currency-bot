ef67c8ac8273d0fb7bff6a1979c3964f5c5f7793
neakylov <akula.kirya@inbox.ru>
2023-03-20 16:02:23 +0300

    Переход на RedisStorage

--- loader.py
+++ loader.py
@@ -1,8 +1,8 @@
 from aiogram import Bot, Dispatcher
 from aiogram.enums import parse_mode
-from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.fsm.storage.redis import RedisStorage
 from data import config
 
 bot = Bot(token=config.BOT_TOKEN, parse_mode=parse_mode.ParseMode.HTML)
-storage = MemoryStorage()
+storage = RedisStorage.from_url(config.REDIS_URL)
 dp = Dispatcher(storage=storage)
