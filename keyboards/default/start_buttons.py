7fcd45774ddb13de68bd8938aa806c961b6936d6
neakylov <akula.kirya@inbox.ru>
2023-03-23 17:29:09 +0300

    Кнопки en/ru

--- keyboards/default/start_buttons.py
+++ keyboards/default/start_buttons.py
@@ -6,16 +6,29 @@ def main():
     pass
 
 
-def start_buttons() -> ReplyKeyboardMarkup:
+def start_buttons_ru() -> ReplyKeyboardMarkup:
     keyboard = ReplyKeyboardBuilder()
     keyboard.add(types.KeyboardButton(text='Курс валют'))
     keyboard.add(types.KeyboardButton(text='Конвертер валют'))
     return keyboard.as_markup(resize_keyboard=True)
 
 
-def cancel_button() -> ReplyKeyboardMarkup:
+def start_buttons_en() -> ReplyKeyboardMarkup:
+    keyboard = ReplyKeyboardBuilder()
+    keyboard.add(types.KeyboardButton(text='Exchange rate'))
+    keyboard.add(types.KeyboardButton(text='Currency converter'))
+    return keyboard.as_markup(resize_keyboard=True)
+
+
+def cancel_button_ru() -> ReplyKeyboardMarkup:
     keyboard = ReplyKeyboardBuilder()
     keyboard.add(types.KeyboardButton(text='Отмена'))
+    return keyboard.as_markup(resize_keyboard=True)
+
+
+def cancel_button_en() -> ReplyKeyboardMarkup:
+    keyboard = ReplyKeyboardBuilder()
+    keyboard.add(types.KeyboardButton(text='Cancel'))
     return keyboard.as_markup(resize_keyboard=True)
 
 
