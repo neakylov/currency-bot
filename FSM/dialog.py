ed5241c494406a3f6dc19ae81b0f0b7fd77f2f0a
neakylov <akula.kirya@inbox.ru>
2023-03-23 17:27:04 +0300

    Поддержка двух языков

--- FSM/dialog.py
+++ FSM/dialog.py
@@ -4,10 +4,12 @@ from typing import Any, Dict
 from data import config
 from loader import dp
 from aiogram import types
-from aiogram.filters import Text
+from aiogram import F
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
-from keyboards.default.start_buttons import start_buttons, cancel_button
+from keyboards.default.start_buttons import start_buttons_ru, start_buttons_en, cancel_button_ru, cancel_button_en
+from keyboards.inline.language import language
+from handlers.handlers import Language
 
 
 class Converter(StatesGroup):
@@ -29,20 +31,45 @@ currencies = ['USD', 'AED', 'AFN', 'ALL', 'AMD', 'ANG', 'AOA', 'ARS', 'AUD', 'AW
               'VND', 'VUV', 'WST', 'XAF', 'XCD', 'XDR', 'XOF', 'XPF', 'YER', 'ZAR', 'ZMW', 'ZWL']
 
 
-@dp.message(Text('Конвертер валют'))
+@dp.message(F.text.in_(['Конвертер валют', 'Currency converter']))
 async def converter(message: types.Message, state: FSMContext) -> None:
+    data = await state.get_data()
+    if 'lang' in data:
+        lang = data['lang']
+    else:
+        lang = None
     await state.set_state(Converter.first_value)
-    await message.answer(f'Выберите валюту, которую хотите конвертировать.\nПример: \"USD\".',
-                         reply_markup=cancel_button())
+    if lang == 'ru':
+        await message.answer(f'Выберите валюту, которую хотите конвертировать.\nПример: \"USD\".',
+                             reply_markup=cancel_button_ru())
+    elif lang == 'en':
+        await message.answer(f'Select the currency you want to convert.\nExample: \"USD\".',
+                             reply_markup=cancel_button_en())
+    else:
+        await message.answer('Choose a language.\nВыберите язык.', reply_markup=language())
 
 
-@dp.message(Text('Отмена'))
+@dp.message(F.text.in_(['Отмена', 'Cancel']))
 async def cancel_handler(message: types.Message, state: FSMContext) -> None:
     current_state = await state.get_state()
     if current_state is None:
         return
+    data = await state.get_data()
+    if 'lang' in data:
+        lang = data['lang']
+    else:
+        lang = None
     await state.clear()
-    await message.answer('Отменено', reply_markup=start_buttons())
+    if lang == 'ru':
+        await state.set_state(Language.lang)
+        await state.update_data(lang=lang)
+        await message.answer('Отменено', reply_markup=start_buttons_ru())
+    elif lang == 'en':
+        await state.set_state(Language.lang)
+        await state.update_data(lang=lang)
+        await message.answer('Cancelled', reply_markup=start_buttons_en())
+    else:
+        await message.answer('Choose a language.\nВыберите язык.', reply_markup=language())
 
 
 @dp.message(Converter.first_value)
@@ -50,7 +77,17 @@ async def first(message: types.Message, state: FSMContext) -> None:
     if message.text.upper() in currencies:
         await state.update_data(first_value=message.text.upper())
         await state.set_state(Converter.quantity)
-        await message.answer('Отправьте количество валюты.\nПример: 13.44')
+        data = await state.get_data()
+        if 'lang' in data:
+            lang = data['lang']
+        else:
+            lang = None
+        if lang == 'ru':
+            await message.answer('Отправьте количество валюты.\nПример: 13.44')
+        elif lang == 'en':
+            await message.answer('Enter the quantity.\nExample: 13.44')
+        else:
+            await message.answer('Choose a language.\nВыберите язык.', reply_markup=language())
     else:
         await state.set_state(Converter.first_value)
         await message.answer(f'Error: InvalidCurrency. Try again.')
@@ -59,6 +96,11 @@ async def first(message: types.Message, state: FSMContext) -> None:
 @dp.message(Converter.quantity)
 async def enter_quantity(message: types.Message, state: FSMContext) -> None:
     message_quantity = []
+    data = await state.get_data()
+    if 'lang' in data:
+        lang = data['lang']
+    else:
+        lang = None
     for i in message.text:
         if i.isdigit():
             message_quantity.append(i)
@@ -69,30 +111,47 @@ async def enter_quantity(message: types.Message, state: FSMContext) -> None:
     result = ''.join(message_quantity)
     if result == '':
         await state.set_state(Converter.quantity)
-        await message.answer('Сообщение должно быть числом.\nПример: 13.44.')
+        if lang == 'ru':
+            await message.answer('Сообщение должно быть числом.\nПример: 13.44.')
+        elif lang == 'en':
+            await message.answer('The message must be a number.\nExample: 13.44.')
+        else:
+            await message.answer('Choose a language.\nВыберите язык.', reply_markup=language())
     else:
         await state.update_data(quantity=float(result))
         await state.set_state(Converter.second_value)
-        await message.answer('Выберите валюту, которую хотите получить.\nПример: \"EUR\".')
+        if lang == 'ru':
+            await message.answer('Выберите валюту, которую хотите получить.\nПример: \"EUR\".')
+        elif lang == 'en':
+            await message.answer('Choose the currency you will receive.\nExample: \"EUR\".')
+        else:
+            await message.answer('Choose a language.\nВыберите язык.', reply_markup=language())
 
 
 @dp.message(Converter.second_value)
 async def second(message: types.Message, state: FSMContext) -> None:
     if message.text.upper() in currencies:
         data = await state.update_data(second_value=message.text.upper())
+        lang = data['lang']
         await state.clear()
-        await converted(message=message, data=data)
+        await state.set_state(Language.lang)
+        await state.update_data(lang=lang)
+        await converted(message=message, data=data, lang=lang)
     else:
         await state.set_state(Converter.second_value)
         await message.answer(f'Error: InvalidCurrency. Try again.')
 
 
-async def converted(message: types.Message, data: Dict[str, Any]) -> None:
+async def converted(message: types.Message, data: Dict[str, Any], lang: str) -> None:
     text = message.text.upper()
     first_curr = data['first_value']
     response = requests.get(config.URL).json()
     from_curr = response.get('conversion_rates')[first_curr]
     to_curr = response.get('conversion_rates')[text]
     quantity = data['quantity']
-    await message.answer(f'Converted: {round((to_curr / from_curr) * float(quantity), 2)} {text}',
-                         reply_markup=start_buttons())
+    if lang == 'en':
+        await message.answer(f'Converted: {round((to_curr / from_curr) * float(quantity), 2)} {text}',
+                             reply_markup=start_buttons_en())
+    elif lang == 'ru':
+        await message.answer(f'Конвертировано: {round((to_curr / from_curr) * float(quantity), 2)} {text}',
+                             reply_markup=start_buttons_ru())
