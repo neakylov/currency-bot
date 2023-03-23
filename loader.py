4bc44f29256be20208bc55ee91ba7b0fe22feb12
neakylov <akula.kirya@inbox.ru>
2023-03-23 17:27:56 +0300

    storage

--- loader.py
+++ loader.py
@@ -1,8 +1,13 @@
 from aiogram import Bot, Dispatcher
 from aiogram.enums import parse_mode
 from aiogram.fsm.storage.redis import RedisStorage
+from aiogram.fsm.storage.memory import MemoryStorage
 from data import config
 
 bot = Bot(token=config.BOT_TOKEN, parse_mode=parse_mode.ParseMode.HTML)
-storage = RedisStorage.from_url(config.REDIS_URL)
+
+if config.REDIS:
+    storage = RedisStorage.from_url(config.REDIS_URL)
+else:
+    storage = MemoryStorage()
 dp = Dispatcher(storage=storage)
