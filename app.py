5ca09cd65c7a676e4c6a2e176feeffe56e713a5d
neakylov <akula.kirya@inbox.ru>
2023-03-23 17:25:26 +0300

    startup and shutdown

--- app.py
+++ app.py
@@ -3,12 +3,15 @@ import handlers
 import FSM
 
 from loader import dp, bot
+from utils.notifications import on_startup, on_shutdown
 
 
 async def main():
+    dp.startup.register(on_startup)
+    dp.shutdown.register(on_shutdown)
     await dp.start_polling(bot)
 
 if __name__ == '__main__':
     asyncio.run(main())
 
-    #asyncio.get_event_loop().run_until_complete(main()) # - if asyncio.run() caused an error
+    # asyncio.get_event_loop().run_until_complete(main()) # - if asyncio.run() caused an error
